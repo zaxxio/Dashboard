8e527624d21f6ac3177a46416830761b516cbaa3
partha <partharaj.dev@gmail.com>
2024-03-31 19:43:39 +0600

    Angular Dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {SubmissionComponent} from "./pages/submission/submission.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: "full", component: DashboardComponent},
+  {path: 'dashboard', pathMatch: "full", component: DashboardComponent},
+  {path: 'submission', pathMatch: "full", component: SubmissionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {MainNavComponent} from './components/main-nav/main-nav.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {SideNavComponent} from './components/main-nav/side-nav/side-nav.component';
+import {MatListModule} from "@angular/material/list";
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SubmissionComponent } from './pages/submission/submission.component';
+import {MatCard, MatCardTitle} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    SideNavComponent,
+    DashboardComponent,
+    SubmissionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCard,
+    MatCardTitle
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
